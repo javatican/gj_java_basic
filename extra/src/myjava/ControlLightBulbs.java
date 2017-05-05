a27c7747462500f45af716b2370b409587a37a30
Ryan Nieh <pon.nieh@gmail.com>
2017-05-05 10:35:24 +0800

    minor

--- extra/src/myjava/ControlLightBulbs.java
+++ extra/src/myjava/ControlLightBulbs.java
@@ -1,14 +1,6 @@
 package myjava;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author ryannieh
- */
+
 public class ControlLightBulbs {
 
     public static int bulbs = 0;
